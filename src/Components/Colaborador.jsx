bf14eff75c0c5bb3046c319d50b7ac06161f21e3
Nelson Henrique <nhsneto@gmail.com>
2023-04-20 17:17:31 -0300

    Adiciona funcoes de formatacao em Colaborador

--- src/Components/Colaborador.jsx
+++ src/Components/Colaborador.jsx
@@ -3,21 +3,64 @@ import { Link } from "react-router-dom";
 import styles from "./Colaborador.module.css";
 
 function Colaborador({ colaborador }) {
+  function formataCPF(cpf) {
+    let cpfFormatado = "";
+
+    for (let i = 0; i < cpf.length; i++) {
+      cpfFormatado += cpf[i];
+      if (i === 2 || i === 5) {
+        cpfFormatado += ".";
+      }
+      if (i === 8) {
+        cpfFormatado += "-";
+      }
+    }
+
+    return cpfFormatado;
+  }
+
+  function formataOpcoes(opcoes) {
+    let opcoesFormatada = "";
+
+    for (let i = 0; i < opcoes.length; i++) {
+      if (i !== opcoes.length - 1) {
+        opcoesFormatada += `${opcoes[i].nome} · `;
+      } else {
+        opcoesFormatada += opcoes[i].nome;
+      }
+    }
+
+    return opcoesFormatada;
+  }
+
+  function formataData(data) {
+    let dia = "";
+    let mes = "";
+    let ano = "";
+
+    [ano, mes, dia] = data.split("-");
+
+    return `${dia}/${mes}/${ano}`;
+  }
+
   return (
     <article className={styles.article}>
       <div className={styles.nomeCpf}>
         <div className={styles.nome}>
           <h2>{colaborador.nome}</h2>
-          <Link to={`/edicao/?id=${colaborador.id}`} className={styles.atualizacao}>
+          <Link
+            to={`/edicao/?id=${colaborador.id}`}
+            className={styles.atualizacao}
+          >
             {<MdCreate />}
           </Link>
         </div>
-        <p className={styles.cpf}>{colaborador.cpf}</p>
+        <p className={styles.cpf}>{formataCPF(colaborador.cpf)}</p>
       </div>
 
-      <p className={styles.opcoes}>{colaborador.opcoes}</p>
+      <p className={styles.opcoes}>{formataOpcoes(colaborador.opcoes)}</p>
 
-      <p className={styles.data}>{colaborador.data}</p>
+      <p className={styles.data}>{formataData(colaborador.data)}</p>
 
       <div>
         <div className={styles.divTrouxe}>
